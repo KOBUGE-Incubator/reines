2807ff1550db7629dc7e28ca1b76b124bcf925d3
alketii <alketii@gmail.com>
2015-06-06 15:07:03 +0200

    added read only to chat

--- scripts/chat_announce.gd
+++ scripts/chat_announce.gd
@@ -3,5 +3,6 @@ extends TextEdit
 
 
 func _ready():
+	set_readonly(true)
 	#this text could be set by a http request, very low priority.
 	set_text("Welcome to Reines. Please set your name and connect to a server.\n")
